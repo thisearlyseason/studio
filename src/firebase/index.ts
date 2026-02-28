62d531d1d31570ae88a4adbb74eea9dcd4ac2228
Tyler Ans <thisearlyseason@gmail.com>
2026-02-28 20:46:10 +0000

    Set up a Firebase backend

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -41,6 +41,7 @@ export function getSdks(firebaseApp: FirebaseApp) {
 }
 
 export * from './provider';
+export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
