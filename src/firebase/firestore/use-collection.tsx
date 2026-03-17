ceea0b9536c699c8a2f328dcfa63d8e2a5c2a14e
Tyler Ans <thisearlyseason@gmail.com>
2026-03-17 04:43:32 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Missing or insuffi

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -29,10 +29,10 @@ export interface UseCollectionResult<T> {
   https://github.com/firebase/firebase-js-sdk/blob/c5f08a9bc5da0d2b0207802c972d53724ccef055/packages/firestore/src/lite-api/reference.ts#L143
 */
 export interface InternalQuery extends Query<DocumentData> {
-  _query: {
-    path: {
-      canonicalString(): string;
-      toString(): string;
+  _query?: {
+    path?: {
+      canonicalString?(): string;
+      toString?(): string;
     }
   }
 }
